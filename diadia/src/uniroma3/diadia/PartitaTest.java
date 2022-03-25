84690739a3d9020201396422463364b10a887c76
MatteoLeme <matteo.mastranza01@gmail.com>
2022-03-25 10:51:43 +0100

    Aggiunta di classi di test Stanza e Partita

---
+++ diadia/src/uniroma3/diadia/PartitaTest.java
@@ -0,0 +1,19 @@
+package uniroma3.diadia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PartitaTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ diadia/src/uniroma3/diadia/StanzaTest.java
@@ -0,0 +1,37 @@
+package uniroma3.diadia;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+public class StanzaTest {
+
+	@Test
+	public void testImpostaStanzaAdiacente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetStanzaAdiacente() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddAttrezzo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testHasAttrezzo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetAttrezzo() {
+		fail("Not yet implemented");
+	}
+
+}
