1ed7a6a13253e33bb28d266d490c4d0182dfdb9d
MatteoLeme <matte.mastranza02@gmail.com>
2022-03-28 16:42:06 +0200

    Indentazione diadia

--- diadia/src/uniroma3/diadia/DiaDia.java
+++ diadia/src/uniroma3/diadia/DiaDia.java
@@ -1,7 +1,5 @@
 package uniroma3.diadia;
 
-
-
 import java.util.Scanner;
 
 /**
@@ -10,25 +8,23 @@ import java.util.Scanner;
  *
  * Questa e' la classe principale crea e istanzia tutte le altre
  *
- * @author  docente di POO 
- *         (da un'idea di Michael Kolling and David J. Barnes) 
- *          
+ * @author docente di POO (da un'idea di Michael Kolling and David J. Barnes)
+ * 
  * @version base
  */
 
 public class DiaDia {
 
-	static final private String MESSAGGIO_BENVENUTO = ""+
-			"Ti trovi nell'Universita', ma oggi e' diversa dal solito...\n" +
-			"Meglio andare al piu' presto in biblioteca a studiare. Ma dov'e'?\n"+
-			"I locali sono popolati da strani personaggi, " +
-			"alcuni amici, altri... chissa!\n"+
-			"Ci sono attrezzi che potrebbero servirti nell'impresa:\n"+
-			"puoi raccoglierli, usarli, posarli quando ti sembrano inutili\n" +
-			"o regalarli se pensi che possano ingraziarti qualcuno.\n\n"+
-			"Per conoscere le istruzioni usa il comando 'aiuto'.";
-	
-	static final private String[] elencoComandi = {"vai", "aiuto", "fine"};
+	static final private String MESSAGGIO_BENVENUTO = ""
+			+ "Ti trovi nell'Universita', ma oggi e' diversa dal solito...\n"
+			+ "Meglio andare al piu' presto in biblioteca a studiare. Ma dov'e'?\n"
+			+ "I locali sono popolati da strani personaggi, " + "alcuni amici, altri... chissa!\n"
+			+ "Ci sono attrezzi che potrebbero servirti nell'impresa:\n"
+			+ "puoi raccoglierli, usarli, posarli quando ti sembrano inutili\n"
+			+ "o regalarli se pensi che possano ingraziarti qualcuno.\n\n"
+			+ "Per conoscere le istruzioni usa il comando 'aiuto'.";
+
+	static final private String[] elencoComandi = { "vai", "aiuto", "fine" };
 
 	private Partita partita;
 
@@ -37,38 +33,38 @@ public class DiaDia {
 	}
 
 	public void gioca() {
-		String istruzione; 
+		String istruzione;
 		Scanner scannerDiLinee;
-		
-		//EDIT 16/03 dopo il messaggio di benvenuto printo dove mi trovo
+
+		// EDIT 16/03 dopo il messaggio di benvenuto printo dove mi trovo
 		System.out.println(MESSAGGIO_BENVENUTO + "\nTi trovi in:\n" + partita.getStanzaCorrente().getDescrizione());
 
-		scannerDiLinee = new Scanner(System.in);		
-		do		
+		scannerDiLinee = new Scanner(System.in);
+		do
 			istruzione = scannerDiLinee.nextLine();
 		while (!processaIstruzione(istruzione));
-	}   
-
+	}
 
 	/**
-	 * Processa una istruzione 
+	 * Processa una istruzione
 	 *
-	 * @return true se l'istruzione e' eseguita e il gioco continua, false altrimenti
+	 * @return true se l'istruzione e' eseguita e il gioco continua, false
+	 *         altrimenti
 	 */
 	private boolean processaIstruzione(String istruzione) {
 		Comando comandoDaEseguire = new Comando(istruzione);
-		
+
 		/*
-		 * EDIT 17/03
-		 * Risolvo il caso in cui l'utente non scriva nulla e digiti solamente invio
+		 * EDIT 17/03 Risolvo il caso in cui l'utente non scriva nulla e digiti
+		 * solamente invio
 		 */
-		
-		if(comandoDaEseguire.getNome() == null) {
+
+		if (comandoDaEseguire.getNome() == null) {
 			this.aiuto();
 			return false;
 		}
 		if (comandoDaEseguire.getNome().equals("fine")) {
-			this.fine(); 
+			this.fine();
 			return true;
 		} else if (comandoDaEseguire.getNome().equals("vai"))
 			this.vai(comandoDaEseguire.getParametro());
@@ -81,7 +77,7 @@ public class DiaDia {
 			return true;
 		} else
 			return false;
-	}   
+	}
 
 	// implementazioni dei comandi dell'utente:
 
@@ -89,19 +85,21 @@ public class DiaDia {
 	 * Stampa informazioni di aiuto.
 	 */
 	private void aiuto() {
-		for(int i=0; i< elencoComandi.length; i++) {
-			System.out.print(elencoComandi[i]+" ");
-			if(i==0) System.out.print("+ (nord, sud, est, ovest) ");					//EDIT 16/03 aggiunta di un aiuto per capire il comando vai
-		}											
+		for (int i = 0; i < elencoComandi.length; i++) {
+			System.out.print(elencoComandi[i] + " ");
+			if (i == 0)
+				System.out.print("+ (nord, sud, est, ovest) "); // EDIT 16/03 aggiunta di un aiuto per capire il comando
+																// vai
+		}
 		System.out.println();
 	}
 
 	/**
-	 * Cerca di andare in una direzione. Se c'e' una stanza ci entra 
-	 * e ne stampa il nome, altrimenti stampa un messaggio di errore
+	 * Cerca di andare in una direzione. Se c'e' una stanza ci entra e ne stampa il
+	 * nome, altrimenti stampa un messaggio di errore
 	 */
 	private void vai(String direzione) {
-		if(direzione==null)
+		if (direzione == null)
 			System.out.println("Dove vuoi andare ?");
 		Stanza prossimaStanza = null;
 		prossimaStanza = this.partita.getStanzaCorrente().getStanzaAdiacente(direzione);
@@ -119,7 +117,7 @@ public class DiaDia {
 	 * Comando "Fine".
 	 */
 	private void fine() {
-		System.out.println("Grazie di aver giocato!");  // si desidera smettere
+		System.out.println("Grazie di aver giocato!"); // si desidera smettere
 	}
 
 	public static void main(String[] argc) {
