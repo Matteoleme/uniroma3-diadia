f94818fe1329575c47c2be6d653dfae228b7cd42
MatteoLeme <matte.mastranza02@gmail.com>
2022-03-28 11:33:00 +0200

    Implementazione classe test classe partita

--- diadia/src/uniroma3/diadia/PartitaTest.java
+++ diadia/src/uniroma3/diadia/PartitaTest.java
@@ -6,14 +6,64 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class PartitaTest {
-
+	
+	private Partita partita;
+	private Partita partita2;
+	private Stanza StanzaVincente;
+	private Stanza StanzaCorrente;
+	private Stanza StanzaTest;
+	
 	@Before
-	public void setUp() throws Exception {
+	public void setUpStanze() {
+		partita = new Partita("Test");
+		partita2 = new Partita("Test");		//Partita senza stanza corrente o vincente
+		StanzaVincente = new Stanza("StanzaVincente");
+		StanzaCorrente = new Stanza("StanzaCorrente");
+		StanzaTest = new Stanza("StanzaTest");
+		
+		partita.setStanzaVincente(StanzaVincente);
+		partita.setStanzaCorrente(StanzaCorrente);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetStanzaVincenteSenza() {
+		assertNull(partita2.getStanzaVincente());
 	}
+	
+	@Test
+	public void testGetStanzaVincente() {
+		assertEquals(StanzaVincente, partita.getStanzaVincente());
+	}
+	
+	@Test
+	public void testGetStanzaCorrenteAggiornato() {
+		partita.setStanzaCorrente(StanzaTest);
+		assertEquals(StanzaTest, partita.getStanzaCorrente());
+	}
+	
+	@Test
+	public void testGetStanzaVincenteAggiornato() {
+		partita.setStanzaVincente(StanzaTest);
+		assertEquals(StanzaTest, partita.getStanzaVincente());
+	}
+	
+	@Test
+	public void testVintaSenzaStanzaVincente() {
+		partita2.setStanzaCorrente(StanzaCorrente);
+		assertEquals(false, partita.vinta());
+	}
+	
+	@Test
+	public void testVintaSenzaStanzaCorrente() {
+		partita2.setStanzaVincente(StanzaVincente);
+		assertEquals(false, partita2.vinta());
+	}
+	
+	@Test
+	public void testVintaSenzaNiente() {
+		assertEquals(false, partita2.vinta());
+	}
+	
+	
 
 }
