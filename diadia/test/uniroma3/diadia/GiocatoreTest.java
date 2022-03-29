d7f0ee498954ea3d54e828920b387f16e72772a6
MatteoLeme <matte.mastranza02@gmail.com>
2022-03-30 01:07:18 +0200

    Implementazione di test su classe Giocatore

--- diadia/test/uniroma3/diadia/GiocatoreTest.java
+++ diadia/test/uniroma3/diadia/GiocatoreTest.java
@@ -6,14 +6,31 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class GiocatoreTest {
-
+	
+	private Giocatore SenzaBorsa;
+	private Giocatore ConBorsa;
+	private Borsa Busta;
+	private Borsa Zaino;
+	
+	
+	
 	@Before
 	public void setUp() throws Exception {
+		SenzaBorsa = new Giocatore("Giovanni");
+		ConBorsa = new Giocatore("Mario");
+		Busta = new Borsa(1);
+		Zaino = new Borsa(10);
+		ConBorsa.setBorsa(Busta);
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetSenzaBorsa() {
+		assertNull(SenzaBorsa.getBorsa());
+	}
+	
+	@Test
+	public void testGetConBorsa() {
+		assertEquals(Busta, ConBorsa.getBorsa());
 	}
 
 }
