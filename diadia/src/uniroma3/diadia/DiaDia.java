7c864191eb79ed40ec31a6c44617d231d878e384
MatteoLeme <matte.mastranza02@gmail.com>
2022-04-14 22:49:58 +0200

    Eliminati i comandi da classe DiaDia

--- diadia/src/uniroma3/diadia/DiaDia.java
+++ diadia/src/uniroma3/diadia/DiaDia.java
@@ -27,13 +27,11 @@ public class DiaDia {
 			+ "o regalarli se pensi che possano ingraziarti qualcuno.\n\n"
 			+ "Per conoscere le istruzioni usa il comando 'aiuto'.";
 
-	static final private String[] elencoComandi = { "vai", "aiuto", "fine", "prendi", "posa" };
-
 	private Partita partita;
 	private IOConsole io;
 
 	public DiaDia(IOConsole io) {
-		this.partita = new Partita("Universita'");
+		this.partita = new Partita("Universita'", io);
 		this.io = io;
 	}
 
@@ -65,64 +63,28 @@ public class DiaDia {
 		 * solamente invio
 		 */
 
-		if (comandoDaEseguire.getNome() == null) {
-			this.aiuto();
-			return false;
-		}
-		if (comandoDaEseguire.getNome().equals("fine")) {
-			this.fine();
-			return true;
-		} else if (comandoDaEseguire.getNome().equals("vai"))
-			this.vai(comandoDaEseguire.getParametro());
-		else if (comandoDaEseguire.getNome().equals("aiuto"))
-			this.aiuto();
-		else if (comandoDaEseguire.getNome().equals("prendi"))
-			this.prendi(comandoDaEseguire.getParametro());
-		else if (comandoDaEseguire.getNome().equals("posa"))
-			this.posa(comandoDaEseguire.getParametro());
-		else
-			io.mostraMessaggioACapo("Comando sconosciuto");
-		if (this.partita.vinta()) {
-			io.mostraMessaggioACapo("Hai vinto!");
-			return true;
-		} else
-			return false;
+		/*
+		 * if (comandoDaEseguire.getNome() == null) { this.aiuto(); return false; } if
+		 * (comandoDaEseguire.getNome().equals("fine")) { this.fine(); return true; }
+		 * else if (comandoDaEseguire.getNome().equals("vai"))
+		 * this.vai(comandoDaEseguire.getParametro()); else if
+		 * (comandoDaEseguire.getNome().equals("aiuto")) this.aiuto(); else if
+		 * (comandoDaEseguire.getNome().equals("prendi"))
+		 * this.prendi(comandoDaEseguire.getParametro()); else if
+		 * (comandoDaEseguire.getNome().equals("posa"))
+		 * this.posa(comandoDaEseguire.getParametro()); else
+		 * io.mostraMessaggioACapo("Comando sconosciuto"); if (this.partita.vinta()) {
+		 * io.mostraMessaggioACapo("Hai vinto!"); return true; } else return false;
+		 */
+		return false;			// da eliminare
 	}
 
 	// implementazioni dei comandi dell'utente:
 
-	private boolean prendi(String nomeAttrezzo) {
-		Stanza stanzaCorrente = this.partita.getStanzaCorrente();
-		if (stanzaCorrente.hasAttrezzo(nomeAttrezzo)) {
-			Attrezzo attrezzoDaPrendere = stanzaCorrente.getAttrezzo(nomeAttrezzo);
-			if (this.partita.getGiocatore().getBorsa().addAttrezzo(attrezzoDaPrendere)) {
-				stanzaCorrente.removeAttrezzo(attrezzoDaPrendere);
-				io.mostraMessaggioACapo(nomeAttrezzo + " preso");
-				return true;
-			} else
-				io.mostraMessaggioACapo("Borsa piena non e' possibile mettere altri attrezzi");
-		} else
-			io.mostraMessaggioACapo(nomeAttrezzo + " non presente");
-		return false;
-	}
-
-	private boolean posa(String nomeAttrezzo) {
-		Borsa borsa = this.partita.getGiocatore().getBorsa();
-		if (borsa.hasAttrezzo(nomeAttrezzo)) {
-			if (this.partita.getStanzaCorrente().addAttrezzo(borsa.getAttrezzo(nomeAttrezzo))) {
-				borsa.removeAttrezzo(nomeAttrezzo);
-				io.mostraMessaggioACapo(nomeAttrezzo + " posato");
-				return true;
-			} else
-				io.mostraMessaggioACapo("Stanza piena non e' possibile mettere altri attrezzi");
-		} else
-			io.mostraMessaggioACapo(nomeAttrezzo + " non presente");
-		return false;
-	}
-
 	// questo metodo diventa necessario per come ho strutturato il codice
 	// e consente inoltre di far impostare all'utente di scegliere il peso della
 	// borsa
+
 	private void creaBorsa(int peso) {
 		Giocatore player = this.partita.getGiocatore();
 		if (player.getBorsa() == null) {
@@ -130,45 +92,6 @@ public class DiaDia {
 			player.setBorsa(borsa);
 		} else
 			io.mostraMessaggioACapo("Il giocatore ha gia' una borsa di " + peso + "kg");
-	}
-
-	/**
-	 * Stampa informazioni di aiuto.
-	 */
-	private void aiuto() {
-		for (int i = 0; i < elencoComandi.length; i++) {
-			io.mostraMessaggio(elencoComandi[i] + " ");
-			if (i == 0)
-				io.mostraMessaggio("+ direzione "); // EDIT 16/03 aggiunta di un aiuto per capire il comando
-													// vai
-		}
-		io.mostraMessaggioACapo("");
-	}
-
-	/**
-	 * Cerca di andare in una direzione. Se c'e' una stanza ci entra e ne stampa il
-	 * nome, altrimenti stampa un messaggio di errore
-	 */
-	private void vai(String direzione) {
-		if (direzione == null)
-			io.mostraMessaggioACapo("Dove vuoi andare ?");
-		Stanza prossimaStanza = null;
-		prossimaStanza = this.partita.getStanzaCorrente().getStanzaAdiacente(direzione);
-		if (prossimaStanza == null)
-			io.mostraMessaggioACapo("Direzione inesistente");
-		else {
-			this.partita.setStanzaCorrente(prossimaStanza);
-			int cfu = this.partita.getGiocatore().getCfu();
-			this.partita.getGiocatore().setCfu(cfu--);
-		}
-		io.mostraMessaggioACapo(partita.getStanzaCorrente().getDescrizione());
-	}
-
-	/**
-	 * Comando "Fine".
-	 */
-	private void fine() {
-		io.mostraMessaggioACapo("Grazie di aver giocato!"); // si desidera smettere
 	}
 
 	public static void main(String[] argc) {
