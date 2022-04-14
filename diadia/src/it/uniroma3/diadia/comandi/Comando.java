5b00bf860f2d25643c16366b7146fc999e6ba1b5
MatteoLeme <matte.mastranza02@gmail.com>
2022-04-14 22:47:36 +0200

    Perfezionamento interfaccia Comando

--- diadia/src/it/uniroma3/diadia/comandi/Comando.java
+++ diadia/src/it/uniroma3/diadia/comandi/Comando.java
@@ -1,5 +1,15 @@
 package it.uniroma3.diadia.comandi;
 
+import uniroma3.diadia.Partita;
+
 public interface Comando {
+	/**
+	 * esecuzione del comando
+	 */
 	public void esegui(Partita partita);
+
+	/**
+	 * set parametro del comando
+	 */
+	public void setParametro(String parametro);
 }
