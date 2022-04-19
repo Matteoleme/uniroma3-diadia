a95d573cfaf65b90dc8f746ff20e6314476a9961
MatteoLeme <matte.mastranza02@gmail.com>
2022-04-19 12:17:22 +0200

    Aggiunta classe StanzaMagica

---
+++ diadia/src/it/uniroma3/diadia/ambienti/StanzaMagica.java
@@ -0,0 +1,49 @@
+package it.uniroma3.diadia.ambienti;
+
+import it.uniroma3.diadia.attrezzi.Attrezzo;
+
+/** 
+ * dopo N volte che in tale stanza viene posato (aggiunto) 
+ * un qualsiasi attrezzo da parte del giocatore, la stanza
+ * inizierà a comportarsi «magicamente»
+ */
+public class StanzaMagica extends Stanza {
+	
+	final static private int SOGLIA_MAGICA_DEFAULT = 3;
+	private int contatoreAttrezziPosati;
+	private int sogliaMagica;
+
+	public StanzaMagica(String nome) {
+		this(nome, SOGLIA_MAGICA_DEFAULT);
+	}
+
+	public StanzaMagica(String nome, int soglia) {
+		super(nome);
+		this.contatoreAttrezziPosati = 0;
+		this.sogliaMagica = soglia;
+	}
+
+	@Override
+	public boolean addAttrezzo(Attrezzo attrezzo) {
+		this.contatoreAttrezziPosati++;
+		if (this.contatoreAttrezziPosati > this.sogliaMagica)
+			attrezzo = this.modificaAttrezzo(attrezzo);
+		return super.addAttrezzo(attrezzo);
+	}
+
+	/**
+	 * la stanza "inverte" il nome dell'attrezzo e ne raddoppia il peso.
+	 * @param attrezzo
+	 * @return attrezzo modificato
+	 */
+	private Attrezzo modificaAttrezzo(Attrezzo attrezzo) {
+		StringBuilder nomeInvertito;
+		int pesoX2 = attrezzo.getPeso() * 2;
+		nomeInvertito = new StringBuilder(attrezzo.getNome());
+		nomeInvertito = nomeInvertito.reverse();
+		attrezzo = new Attrezzo(nomeInvertito.toString(), pesoX2);
+		return attrezzo;
+
+	}
+
+}
