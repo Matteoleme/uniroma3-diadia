1e06407208e637a8f876d85e21cd8e17fee8d310
MatteoLeme <matte.mastranza02@gmail.com>
2022-04-19 12:05:42 +0200

    Aggiunta classe di test per FabbricaDomandiFis

---
+++ diadia/test/it/uniroma3/diadia/comandi/FabbricaDiComandoFisarmonicaTest.java
@@ -0,0 +1,58 @@
+package it.uniroma3.diadia.comandi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FabbricaDiComandoFisarmonicaTest {
+	
+	private String tuttoIlComando;
+	private Comando comando;
+	private FabbricaDiComandi fab;
+	
+	@Before
+	public void setUp() {
+		fab = new FabbricaDiComandoFisarmonica();
+	}
+	
+	@Test
+	public void testComandoGenericoSenzaParametro() {
+		tuttoIlComando = "vai";
+		Comando nuovo = fab.costruisciComando(tuttoIlComando);
+		assertEquals("Vai", nuovo.getNome());
+	}
+	
+	@Test
+	public void testComandoGenericoConParametro() {
+		tuttoIlComando = "vai nord";
+		assertEquals("Vai", fab.costruisciComando(tuttoIlComando).getNome());
+	}
+
+	@Test
+	public void testParametroDiComando() {
+		tuttoIlComando = "vai nord";
+		assertEquals("nord", fab.costruisciComando(tuttoIlComando).getParametro());
+	}
+	
+	@Test
+	public void testComandoNonValido() {
+		tuttoIlComando = "voi";
+		assertEquals("NonValido", fab.costruisciComando(tuttoIlComando).getNome());
+	}
+	
+	@Test
+	public void testComandoCheNonHaParametroConParametro() {
+		tuttoIlComando = "guarda qui";
+		assertEquals("Guarda", fab.costruisciComando(tuttoIlComando).getNome());
+	}
+	
+	@Test
+	public void testComandoCheHaParametroSenzaParametro() {
+		tuttoIlComando = "vai";
+		assertNull(fab.costruisciComando(tuttoIlComando).getParametro());
+	}
+	
+	
+	
+}
