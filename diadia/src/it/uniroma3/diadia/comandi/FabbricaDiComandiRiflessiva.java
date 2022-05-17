bdfd840449956372c826f5e9aaf759e25041fac2
MatteoLeme <matte.mastranza02@gmail.com>
2022-05-17 15:49:32 +0200

    Implementazione di una nuova fabbrica di comandi con introspezione

---
+++ diadia/src/it/uniroma3/diadia/comandi/FabbricaDiComandiRiflessiva.java
@@ -0,0 +1,25 @@
+package it.uniroma3.diadia.comandi;
+
+import java.util.Scanner;
+
+public class FabbricaDiComandiRiflessiva implements FabbricaDiComandi {
+	public Comando costruisciComando(String istruzione) throws Exception {
+		Scanner scannerDiParole = new Scanner(istruzione); // es. ‘vai sud’
+		String nomeComando = null; // es. ‘vai’
+		String parametro = null; // es. ‘sud’
+		Comando comando = null;
+
+		if (scannerDiParole.hasNext())
+			nomeComando = scannerDiParole.next();// prima parola: nome del comando
+		if (scannerDiParole.hasNext())
+			parametro = scannerDiParole.next();// seconda parola: eventuale parametro
+		StringBuilder nomeClasse = new StringBuilder("it.uniroma3.diadia.comandi.Comando");
+		nomeClasse.append(Character.toUpperCase(nomeComando.charAt(0)));
+		// es. nomeClasse: ‘it.uniroma3.diadia.comandi.ComandoV’
+		nomeClasse.append(nomeComando.substring(1));
+		// es. nomeClasse: ‘it.uniroma3.diadia.comandi.ComandoVai’
+		comando = (Comando) Class.forName(nomeClasse.toString()).newInstance();
+		comando.setParametro(parametro);
+		return comando;
+	}
+}
