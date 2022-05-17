f5f46495acdd143c73bc950281a9d67e6f9857d8
MatteoLeme <matte.mastranza02@gmail.com>
2022-05-17 15:50:21 +0200

    cambio versione di java per compilare (da 11 a 7)

--- diadia/src/it/uniroma3/diadia/comandi/FabbricaDiComandiRiflessiva.java
+++ diadia/src/it/uniroma3/diadia/comandi/FabbricaDiComandiRiflessiva.java
@@ -5,8 +5,8 @@ import java.util.Scanner;
 public class FabbricaDiComandiRiflessiva implements FabbricaDiComandi {
 	public Comando costruisciComando(String istruzione) throws Exception {
 		Scanner scannerDiParole = new Scanner(istruzione); // es. ‘vai sud’
-		String nomeComando = null; // es. ‘vai’
-		String parametro = null; // es. ‘sud’
+		String nomeComando = null; // es. 'vai'
+		String parametro = null; // es. 'sud'
 		Comando comando = null;
 
 		if (scannerDiParole.hasNext())
