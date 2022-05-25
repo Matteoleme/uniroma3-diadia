5f10f09a54b791e3b1728b4860f122464954d00a
MatteoLeme <matte.mastranza02@gmail.com>
2022-05-25 13:59:57 +0200

    Aggiunte classi per personaggi nuovi

---
+++ diadia/src/it/uniroma3/diadia/personaggi/Cane.java
@@ -0,0 +1,18 @@
+package it.uniroma3.diadia.personaggi;
+
+import uniroma3.diadia.Partita;
+
+public class Cane extends AbstractPersonaggio {
+
+	public Cane(String nome, String presentaz) {
+		super(nome, presentaz);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String agisci(Partita partita) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ diadia/src/it/uniroma3/diadia/personaggi/Strega.java
@@ -0,0 +1,18 @@
+package it.uniroma3.diadia.personaggi;
+
+import uniroma3.diadia.Partita;
+
+public class Strega extends AbstractPersonaggio {
+
+	public Strega(String nome, String presentaz) {
+		super(nome, presentaz);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public String agisci(Partita partita) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
